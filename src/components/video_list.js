deac2a9a3df9a7c5489fa7905d13843f12751f62
herze <herzed@gmail.com>
2017-10-17 07:10:39 -0400

    Agregado funcionalidaD Continuando con el ejemplo.

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,15 +1,14 @@
 import React, {Component} from 'react';
-
+import VideoListItem from './video_list_item';
 class VideoList extends Component{
     constructor(props){
         super(props)
     }
     render(){
+        const lista = this.props.videos.map((video,id)=><VideoListItem key={id} video = {video}/>)
         return(
             <ul className="col-md-4 list-group">
-                {this.props.videos.map((video,id)=>{
-                    return <li key={id}>hola</li>
-                })}                
+                {lista}
             </ul>
         )
     }
---
+++ src/components/video_list_item.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class VideoListItem extends Component{
+    render(){
+        return(
+            <li>
+                Video
+            </li>
+        )
+    }
+}
+
+export default VideoListItem;
